a149de2ed190e17c7873daee2273e55b928da415
Robert Jensen <jensen.robert@gmail.com>
2016-12-13 13:53:59 +0100

    Added alarm mode

--- hue.py
+++ hue.py
@@ -1,17 +1,58 @@
 """ Philips Hue Light control """
-from qhue import Bridge
+from __future__ import print_function
+import threading
+import time
+from qhue import Bridge  # pylint: disable=F0401
+import wiringpi as wp # pylint: disable=F0401
+import hue_bulb
 
 BRIDGE_ADRESS = '192.168.1.3'
 USERNAME = 'Ren8Zo7rcIHUCpqI-yTjLGrJBBBolWIpO4B-Qzg4'
 
-def main():
-    bridge = Bridge(BRIDGE_ADRESS, USERNAME)
+class HueControl(threading.Thread):
+    """ App for controlling a set of Hue Bulbs """
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.bridge = Bridge(BRIDGE_ADRESS, USERNAME)
+        self.alarm_mode = False
+        self.quit = False
+        lights = self.bridge.lights
+        self.bulbs = {}
+        for key in list(lights().keys()):
+            print(key)
+            bulb = lights[key]
+            self.bulbs[key] = hue_bulb.HueBulb(bulb)
+        #vindue.state(bri=255)
 
-    lights = bridge.lights
-    vindue = lights[14]
-    print(vindue())
-    #vindue.state(bri=255)
-    vindue.state(on=False)
+    def turn_on_alarm_mode(self):
+        """ Keep an eye on movement and turn on all lights """
+        wp.wiringPiSetup()
+        self.alarm_mode = True
+        print('Alarm mode_activated')
+
+    def turn_all_on_off(self, turn_on, notify=False):
+        """ Turn all bulbs on or off
+        If notify is true, a message will be send"""
+        for bulb in self.bulbs:
+            self.bulbs[bulb].turn_on_off(turn_on)
+        if notify:
+            pass
+        
+    def run(self):
+        while not self.quit:
+            time.sleep(1)
+            print('!')
+            if self.alarm_mode:
+                movement = wp.digitalRead(0) != 0
+                if movement:
+                    self.turn_all_on_off(True, notify=True)
+
+
+
 if __name__ == '__main__':
-
-    main()
+    HUE = HueControl()
+    HUE.start()
+    HUE.turn_all_on_off(False)
+    time.sleep(5)
+    HUE.turn_on_alarm_mode()
+    
--- hue_bulb.py
+++ hue_bulb.py
@@ -2,15 +2,26 @@
 
 import threading
 import time
-#from qhue import Bridge
 
 class HueBulb(threading.Thread):
     """ An instance of a Philips Hue Bulb """
-    def __init__(self, bridge):
+    def __init__(self, bulb):
         threading.Thread.__init__(self)
-        self.bridge = bridge
+        self.bulb = bulb
+        self.state = self.bulb()
         self.quit = False
+
+    def reachable(self):
+        """ Returns true if the bulb is avaiable on the network """
+        return self.state['state']['reachable']
+
+    def turn_on_off(self, on_off_state):
+        """ Turns the bulb on or off """
+        if self.reachable():
+            self.bulb.state(on=on_off_state)
+        return self.reachable()
         
     def run(self):
         while not self.quit:
-            time.sleep(1)
+            time.sleep(5)
+            self.state = self.bulb()
